0fbe71f02ccb5325167ee9dddf2cbe352f2456bd
Myles Atherstone <myles@mylesatherstone.com>
2022-07-14 11:55:25 +0100

    Update sidebar style

--- components/containers/sidebar/sidebar.js
+++ components/containers/sidebar/sidebar.js
@@ -1,3 +1,9 @@
 export default function Sidebar({ children }) {
-  return <div className="sidebar">{children}</div>;
+  return (
+    <div className="sidebar">
+      <div className="radial-gradient-top"></div>
+      <div className="radial-gradient-bottom"></div>
+      <div className="content">{children}</div>
+    </div>
+  );
 }
