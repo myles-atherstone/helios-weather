5e13f1cdf652f0f56dcf01ca83fccd394b541f04
Myles Atherstone <myles@mylesatherstone.com>
2022-07-15 14:14:36 +0100

    Disable react strict mode

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
 };
 
